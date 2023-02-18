2bf88d5f1f11f6a4062475b2c220683cb8b2ab11
valery <vagechirkov@gmail.com>
2023-02-18 15:19:56 +0100

    add traces context

---
+++ trace-viewer/src/context/TracesContext.tsx
@@ -0,0 +1,49 @@
+import {createContext, FC, useContext, useEffect, useReducer} from "react";
+import tracesReducer from "@/context/TracesReducer";
+
+const LOCAL_STORAGE_TRACES_STATE_KEY = 'networkState';
+
+export type TracesState = {
+    traces: any[];
+}
+
+export type TracesContextType = {
+    tracesState: TracesState;
+    tracesDispatcher: (action: any) => void;
+};
+
+const TraceContext = createContext<TracesContextType | null>(null);
+
+export const tracesInitialState: TracesState = {
+    traces: []
+}
+
+const tracesInitializer = (initialState: TracesState) => {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_TRACES_STATE_KEY) || '{}') || initialState;
+}
+
+interface ITracesContextProvider {
+    children: any;
+    saveToLocalStorage?: boolean;
+}
+
+export const NetworkContextProvider: FC<ITracesContextProvider> = (props) => {
+    const {children, saveToLocalStorage=true} = props;
+    const [state, dispatch] = useReducer(tracesReducer, tracesInitialState, tracesInitializer);
+
+    useEffect(() => {
+        if(saveToLocalStorage)
+            localStorage.setItem(LOCAL_STORAGE_TRACES_STATE_KEY, JSON.stringify(state));
+    }, [state]);
+
+    return (
+        <TraceContext.Provider value={{tracesState: state, tracesDispatcher: dispatch}}>
+            {children}
+        </TraceContext.Provider>
+    );
+};
+
+
+const useTraceContext = () => useContext(TraceContext);
+
+export default useTraceContext;
---
+++ trace-viewer/src/context/TracesReducer.tsx
@@ -0,0 +1,8 @@
+import {TracesState} from "@/context/TracesContext";
+
+
+const tracesReducer = (state: TracesState, action: any) => {
+    return state;
+}
+
+export default tracesReducer;
