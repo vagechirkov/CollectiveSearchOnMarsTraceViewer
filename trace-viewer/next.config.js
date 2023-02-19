f541f99905c6db7c078b62eb776f5f49a5e4451f
valery <vagechirkov@gmail.com>
2023-02-19 22:12:02 +0100

    fix local config

--- trace-viewer/next.config.js
+++ trace-viewer/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/CollectiveSearchOnMarsTraceViewer/',
+  assetPrefix: isProd ? '/CollectiveSearchOnMarsTraceViewer/' : '',
 }
 
 module.exports = nextConfig
