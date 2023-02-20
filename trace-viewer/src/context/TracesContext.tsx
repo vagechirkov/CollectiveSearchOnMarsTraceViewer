377b3ef84086c2da5250c44d3fa9353ae5edfbc8
valery <vagechirkov@gmail.com>
2023-02-20 09:48:53 +0100

    fetch files from github

--- trace-viewer/src/context/TracesContext.tsx
+++ trace-viewer/src/context/TracesContext.tsx
@@ -1,5 +1,14 @@
-import {createContext, FC, useContext, useReducer} from "react";
-import tracesReducer from "@/context/TracesReducer";
+import {createContext, FC, useContext, useEffect, useReducer} from "react";
+import tracesReducer, {ACTIONS} from "@/context/TracesReducer";
+
+const examples = [
+    "ex_1_alone_resource_1DE846936D166543_0_traces.txt",
+    "ex_2_multi_2_resource_156A4894999DCEE2_33144C1FA5587E4C_0_traces.txt",
+    "ex_3_multi_2_resource_32FA06AE976FB58D_EE5B78DA65AE5B21_0_traces.txt"
+]
+
+const baseUrl = 'https://raw.githubusercontent.com/vagechirkov/CollectiveSearchOnMarsTraceViewer/main/examples/';
+
 
 const LOCAL_STORAGE_TRACES_STATE_KEY = 'networkState';
 
@@ -46,13 +55,31 @@ interface ITracesContextProvider {
 }
 
 export const TracesContextProvider: FC<ITracesContextProvider> = (props) => {
-    const {children, saveToLocalStorage=true} = props;
+    const {children, saveToLocalStorage = true} = props;
     const [state, dispatch] = useReducer(tracesReducer, tracesInitialState, tracesInitializer);
 
     // useEffect(() => {
     //     if(saveToLocalStorage)
     //         localStorage.setItem(LOCAL_STORAGE_TRACES_STATE_KEY, JSON.stringify(state));
     // }, [state]);
+
+    useEffect(() => {
+        if (!state.files) {
+            // fetch some example files from GitHub
+            for (const f in examples) {
+                const file = examples[f];
+                const url = baseUrl + file;
+                fetch(url)
+                    .then(d => d.text())
+                    .then(d => dispatch({
+                            type: ACTIONS.ADD_FILE,
+                            payload: {file: {name: file, content: d} as traceFile}
+                        })
+                    );
+            }
+        }
+
+    }, [state.files]);
 
     return (
         <TraceContext.Provider value={{tracesState: state, tracesDispatcher: dispatch}}>
